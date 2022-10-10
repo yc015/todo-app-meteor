c75df5fc97e91041f722d36a14e724b30b4d6823
Yida Chen <yc015@bucknell.edu>
2022-10-09 23:44:12 -0400

    upload the todo app

---
+++ imports/ui/App.svelte
@@ -0,0 +1,54 @@
+<script>
+  import Task from './Task.svelte';
+  import { TasksCollection } from '../api/TasksCollection';
+  import TaskForm from './TaskForm.svelte';
+
+  let hideCompleted = false;
+  
+  const setHideCompleted = value =>  {
+        hideCompleted = value;
+  }
+
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+
+  let incompleteCount;
+  let pendingTasksTitle = '';
+  let tasks = [];
+
+  $m: {
+      tasks = TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, { sort: { createdAt: -1 } }).fetch()
+
+      incompleteCount = TasksCollection.find(hideCompletedFilter).count();
+
+      pendingTasksTitle = `${
+              incompleteCount ? ` (${incompleteCount})` : ''
+      }`;
+  }
+
+</script>
+
+
+<div class="app">
+  <header>
+    <div class="app-bar">
+        <div class="app-header">
+            <h1>📝️ To Do List {pendingTasksTitle}</h1>
+        </div>
+    </div>
+  </header>
+
+  <div class="main">
+      <TaskForm />
+      <div class="filter">
+          <button on:click={() => setHideCompleted(!hideCompleted)}>
+              {hideCompleted ? 'Show All' : 'Hide Completed'}
+          </button>
+      </div>
+
+      <ul class="tasks">
+          {#each tasks as task (task._id)}
+              <Task task={task} />
+          {/each}
+      </ul>
+  </div>
+</div>
